83ff1f292548345a01938546fcc1667997422b39
Abhishek D Jaiwar <devabhishekjaiswar@gmail.com>
2025-04-21 12:57:39 +0530

    Deploy the application to render.com

--- app/camera.py
+++ app/camera.py
@@ -5,8 +5,6 @@ haarcascade_location = cv2.data.haarcascades
 plate_detection1 = cv2.CascadeClassifier(haarcascade_location + 'haarcascade_russian_plate_number.xml')
 plate_detection2 = cv2.CascadeClassifier(haarcascade_location + 'haarcascade_license_plate_rus_16stages.xml')
 
-frame_with_roi = None
-
 class VideoCamera(object):
     def __init__(self):
         self.video = cv2.VideoCapture(0)
@@ -14,20 +12,18 @@ class VideoCamera(object):
 
     def __del__(self):
         self.video.release()
-    
-    def get_frame_plate(self):
-        global frame_with_roi
 
+    def get_frame_plate(self):
         minArea = 500
+        frame_with_roi = None  # use local variable
 
         success, frame = self.video.read()
         if not success:
             print('Error camera: failed to grab frame')
-            return False
+            return None, None
 
         # Flip the frame vertically
-        frame = cv2.flip(frame, 0)
-
+        # frame = cv2.flip(frame, 0)
         frameGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         numberPlates = plate_detection1.detectMultiScale(frameGray, scaleFactor=1.1, minNeighbors=4)
 
@@ -37,12 +33,11 @@ class VideoCamera(object):
                 frame_with_roi = frame[y:y + h, x:x + w]
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
                 cv2.putText(frame, 'Number Plate', (x, y - 5), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
-        
+
         ret, jpeg_frame = cv2.imencode('.jpg', frame)
 
         if frame_with_roi is not None:
             ret, jpeg_roi = cv2.imencode('.jpg', frame_with_roi)
+            return jpeg_frame.tobytes(), jpeg_roi.tobytes()
         else:
-            jpeg_roi = None
-
-        return jpeg_frame.tobytes(), jpeg_roi.tobytes() if jpeg_roi is not None else None
+            return jpeg_frame.tobytes(), None
--- app/settings.py
+++ app/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+# Import dj-database-url at the beginning of the file.
+import dj_database_url
+import os
 from pathlib import Path
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,15 +24,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m_)pizposk6=8xq=@8!@^ql-t(9u35c!mugca5a@&0fdw=dbxt'
+# SECRET_KEY = 'django-insecure-m_)pizposk6=8xq=@8!@^ql-t(9u35c!mugca5a@&0fdw=dbxt'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = False
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = [
-    "*",
-    "0.0.0.0",
-]
+# ALLOWED_HOSTS = [
+#     "*",
+#     "0.0.0.0",
+# ]
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
@@ -54,6 +61,7 @@ INTERNAL_IPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -87,13 +95,31 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# Replace the SQLite DATABASES configuration with PostgreSQL:
 DATABASES = {
+    # 'default': dj_database_url.config(
+    #     # Replace this value with your local database's connection string.
+    #     # default='postgresql://postgres:postgres@localhost:5432/number-plate-detection-system-db',
+
+    #     default='postgresql://number_plate_detection_system_database_user:EXPFyqntD90njxC0XvbdXKX3QGPEFsHp@dpg-d02f3ujuibrs73aqmuk0-a.oregon-postgres.render.com/number_plate_detection_system_database',
+    #     conn_max_age=600
+    # )
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST', default='localhost'),
+        'PORT': config('PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -127,9 +153,19 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# This setting informs Django of the URI path from which your static files will be served to users
+# Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
+STATIC_URL = '/static/'
+
+# This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
